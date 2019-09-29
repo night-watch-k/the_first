f4fdb55a35531cc7c40caccbb245f56c238917a0
admin <admin@localhost.localdomain>
2019-09-29 19:28:07 +0800

    first add my code for test

---
+++ Makefile
@@ -0,0 +1,5 @@
+first:first.c
+	gcc -o first first.c
+.PHONY:clean
+clean:
+	rm -f first
---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+ int i=0;
+ for(;i<100;i++)
+ {
+  printf("hello :%d\n",i);
+ }
+ return 0;
+}
